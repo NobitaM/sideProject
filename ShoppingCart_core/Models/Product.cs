621bb6233c4d7435621330a18b51bc0d68c18a25
mingze-mac <js923030238@hotmail.com>
2021-01-07 11:46:34 +0800

    購物車sideProject git上傳測試

---
+++ ShoppingCart_core/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart_core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
